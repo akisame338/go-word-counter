c3af1978f2cc840b490f74e4c5449b6399550c89
akisame338 <akisame338@gmail.com>
2018-01-01 20:31:35 +0900

    形態素解析を実行できるように修正

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "strings"
+    "github.com/ikawaha/kagome/tokenizer"
+)
 
 func main() {
-    fmt.Println("Hello, world!\n")
+    dic := tokenizer.SysDicSimple()
+    t := tokenizer.NewWithDic(dic)
+    tokens := t.Tokenize("すもももももももものうち")
+    for _, token := range tokens {
+        features := strings.Join(token.Features(), ",")
+        fmt.Printf("%s\t%v\n", token.Surface, features)
+    }
 }
